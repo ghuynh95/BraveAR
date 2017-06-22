96b1986d7e4ae3adf3cd07fadfc839760d2c151b
Arjun Chib <arjunpatrickchib@gmail.com>
2017-06-22 11:08:46 -0400

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+// initialize Argon
+var app = Argon.init();
+app.context.setDefaultReferenceFrame(app.context.localOriginEastUpSouth);
+
+// initialize THREE
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera();
+var userLocation = new THREE.Object3D;
+scene.add(camera);
+scene.add(userLocation);
+var renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    logarithmicDepthBuffer: true
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+app.view.element.appendChild(renderer.domElement);
