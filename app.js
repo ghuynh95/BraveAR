c6d95d8edbc81b4e905e816dab6dfd943ccf2546
kashyap@codemettle.com <kashyap@codemettle.com>
2017-06-22 19:48:03 -0400

    Added section/row + coordinate methods to app.js

--- app.js
+++ app.js
@@ -1,16 +1,54 @@
-// initialize Argon
-var app = Argon.init();
-app.context.setDefaultReferenceFrame(app.context.localOriginEastUpSouth);
+function initialize() {
+	// initialize Argon
+	app = Argon.init();
+	app.context.setDefaultReferenceFrame(app.context.localOriginEastUpSouth);
 
-// initialize THREE
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera();
-var userLocation = new THREE.Object3D;
-scene.add(camera);
-scene.add(userLocation);
-var renderer = new THREE.WebGLRenderer({
-    alpha: true,
-    logarithmicDepthBuffer: true
-});
-renderer.setPixelRatio(window.devicePixelRatio);
-app.view.element.appendChild(renderer.domElement);
+	// initialize THREE
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera();
+	userLocation = new THREE.Object3D;
+	scene.add(camera);
+	scene.add(userLocation);
+	renderer = new THREE.WebGLRenderer({
+	    alpha: true,
+	    logarithmicDepthBuffer: true
+	});
+	renderer.setPixelRatio(window.devicePixelRatio);
+	app.view.element.appendChild(renderer.domElement);
+
+	createCoordinateMap();
+
+}
+
+function getSectionAndRow() {
+	sectionElement = document.getElementById('sectionInput');
+	rowElement = document.getElementById('rowInput');
+
+	form = document.getElementById('form');
+
+	section = sectionElement.value;
+	row = rowElement.value;
+
+	form.style.display = 'none';
+
+	var coordinates = getCoordinates(section, row);
+	console.log(coordinates);
+}
+
+//gets coordinates from dictionary with section and row as keys
+function getCoordinates(section, row) {
+	var secrow = section + "," + row;
+	var coord = dictionary[secrow];
+	if (coord == "") {
+		console.log("Error getting coordinates");
+	} else {
+		return coord;
+	}
+}
+
+function createCoordinateMap() {
+	dictionary = {};
+	dictionary["20,10"] = "0,0,0";
+	dictionary["30,20"] = "1,1,1";
+
+}
