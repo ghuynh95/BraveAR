e3c5ccea390f817f13353e8a643e51c87434e8a5
Kashyap Patel <kpatel@it-test-mbp.dispersive.local>
2017-06-23 15:57:06 -0400

    Added images and coordinates of phone, working on placing coordinates at bottom (as footer)

--- app.js
+++ app.js
@@ -16,9 +16,50 @@ function initialize() {
 	renderer.setPixelRatio(window.devicePixelRatio);
 	app.view.element.appendChild(renderer.domElement);
 
-	createCoordinateMap();
+	// createCoordinateMap();
+	locate();
 
 }
+
+function locate() {
+
+	var output = document.getElementById('location');
+
+	if (!navigator.geolocation) {
+		console.log("No such navigator");
+		output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+		return;
+	}
+
+	function success(position) {
+		console.log("Success");
+		var latitude = position.coords.latitude;
+		var longitude = position.coords.longitude;
+
+		output.innerHTML = "<p>Latitude: " + latitude + "° <br>Longitude: " + longitude + "°</p>";
+	}
+
+	function error() {
+		console.log("Error");
+		output.innerHTML = "Unable to retrieve your location";
+	}
+
+	output.innerHTML = "<p>Locating...</p>";
+
+	navigator.geolocation.getCurrentPosition(success, error);
+
+
+}
+
+
+
+
+
+
+
+
+// under this commend are functions for obtaining a user's coordinates via section and row
+
 
 function getSectionAndRow() {
 	sectionElement = document.getElementById('sectionInput');
